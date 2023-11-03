b2df93ca8298006f54080d2fa0c7bc2bb39e6de6
NunaTna <tseganesh.abera@outlook.com>
2023-11-03 09:20:54 +0300

    Conversation app

--- Keguadachin/urls.py
+++ Keguadachin/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', include('core.urls')),
     path('items/', include('item.urls')),
     path('home/', include('home.urls')),
+    path('inbox/', include('conversation.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
